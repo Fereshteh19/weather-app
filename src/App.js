9c4b3845260344135eb6c8a20b8d3858fc190536
Fereshteh19 <127944580+Fereshteh19@users.noreply.github.com>
2023-11-21 21:04:25 +0330

    change width of main border

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ import WeatherSearch from './weatherSearch';
 
 function App() {
   return (
-  
+ 
     <div className="App">
       
        <h1>Weather App</h1>
        <WeatherSearch  />
+
+       <a href='https://github.com/Fereshteh19/weather-app'
+        rel='weather-app-source'>
+           Open-source code Weather App
+       </a>
     </div>
-    <a href='https://github.com/Fereshteh19/weather-app' rel='weather-app-source'>Open-source code Weather App</a>
+   
     
 
   );
